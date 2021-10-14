7ca06417272165eb6f34635750cb08d770ca95b8
Terrence Huang <terrencehuang2000@gmail.com>
2021-10-14 21:59:32 +0800

    Add fake data

--- core/components/CryptoList.jsx
+++ core/components/CryptoList.jsx
@@ -2,6 +2,25 @@ import React from "react";
 import { View, Text } from "react-native";
 import CryptoListItem from "./CryptoListItem";
 
+// TODO: Fake data need to remove and use the REST to get the real one
+const DATA = [
+  {
+    id: "bitcoin",
+    name: "Bitcoin",
+    current_price: 57793,
+  },
+  {
+    id: "ethereum",
+    name: "Ethereum",
+    current_price: 3656.33,
+  },
+  {
+    id: "binancecoin",
+    name: "Binance Coin",
+    current_price: 480.11,
+  },
+];
+
 const CryptoList = () => {
   return (
     <View>
