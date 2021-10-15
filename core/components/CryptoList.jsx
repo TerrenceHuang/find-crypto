4ea801fa0ee9051796dc2c0348fe593d1151adbf
Terrence Huang <terrencehuang2000@gmail.com>
2021-10-15 13:50:15 +0800

    Fetch the first page of coin markets data

--- core/components/CryptoList.jsx
+++ core/components/CryptoList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList } from "react-native";
 
 import CryptoListItem from "./CryptoListItem";
@@ -30,14 +30,26 @@ const DATA = [
       "https://assets.coingecko.com/coins/images/825/large/binance-coin-logo.png?1547034615",
   },
 ];
+import { getCoinsMarkets } from "../utils/CoinGecko";
 
 const CryptoList = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getCoinsMarkets({ vsCurrency: "usd", perPage: 25, page: 1 })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => alert(error));
+  }, []);
+
   const renderListItem = ({ item }) => {
     return <CryptoListItem {...item} />;
   };
+
   return (
     <FlatList
-      data={DATA}
+      data={data}
       renderItem={renderListItem}
       keyExtractor={(item) => item.id}
     />
