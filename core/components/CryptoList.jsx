f0f98518d26d493cc791d4def919d4bbc230aba5
Terrence Huang <terrencehuang2000@gmail.com>
2021-10-15 13:50:31 +0800

    Remove unused mock data

--- core/components/CryptoList.jsx
+++ core/components/CryptoList.jsx
@@ -2,34 +2,6 @@ import React, { useEffect, useState } from "react";
 import { FlatList } from "react-native";
 
 import CryptoListItem from "./CryptoListItem";
-
-// TODO: Fake data need to remove and use the REST to get the real one
-const DATA = [
-  {
-    id: "bitcoin",
-    name: "Bitcoin",
-    current_price: 57793,
-    total_volume: 39208187835,
-    image:
-      "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
-  },
-  {
-    id: "ethereum",
-    name: "Ethereum",
-    current_price: 3656.33,
-    total_volume: 22661498389,
-    image:
-      "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880",
-  },
-  {
-    id: "binancecoin",
-    name: "Binance Coin",
-    current_price: 480.11,
-    total_volume: 2903013490,
-    image:
-      "https://assets.coingecko.com/coins/images/825/large/binance-coin-logo.png?1547034615",
-  },
-];
 import { getCoinsMarkets } from "../utils/CoinGecko";
 
 const CryptoList = () => {
